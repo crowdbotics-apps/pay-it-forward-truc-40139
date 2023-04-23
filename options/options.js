4f0e798f0e6927ca132c062abfeec83d08589b8c
Crowdbotics <team@crowdbotics.com>
2023-04-23 12:51:27 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "pay-it-forward-truc-40139",
+  url: "https://pay-it-forward-truc-40139.botics.co",
+  api: "https://pay-it-forward-truc-40139.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
